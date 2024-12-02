fd0ad4d7db164f0158a2670229e3c00279234825
timpengu <tim@peugniez.com>
2024-12-02 16:38:58 +0000

    Created project and Day 1 solution (24 hours ago)

---
+++ AoC-2024/AoC-2024-01/Program.cs
@@ -0,0 +1,16 @@
+﻿
+List<int[]> ls = new(
+    File.ReadLines("input.txt")
+        .Select(line => line
+            .Split(default(char[]), 2, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray()       
+    ));
+
+var xs = ls.Select(l => l[0]);
+var ys = ls.Select(l => l[1]);
+
+int distance = xs.Order().Zip(ys.Order(), (x, y) => Math.Abs(x - y)).Sum();
+int similarity = xs.Sum(x => x * ys.Count(y => x == y));
+
+Console.WriteLine($"{distance} {similarity}");
