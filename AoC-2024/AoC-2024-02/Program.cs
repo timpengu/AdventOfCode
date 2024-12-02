b81c8956512ec3eab263f0f0e69d0729fb369306
timpengu <tim@peugniez.com>
2024-12-02 14:36:40 +0000

    Day 02: added solution

---
+++ AoC-2024/AoC-2024-02/Program.cs
@@ -0,0 +1,30 @@
+﻿using MoreLinq;
+
+internal static class Program
+{
+    static void Main(string[] args)
+    {
+        List<List<int>> ls = new(
+            File.ReadLines("input.txt")
+                .Select(line => line
+                    .Split(default(char[]), StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList()
+        ));
+
+        int safeCount0 = ls.Count(l => l.IsSafe());
+        
+        int safeCount1 = ls.Count(l =>
+            l.IsSafe() ||
+            Enumerable.Range(0, l.Count).Any(index => l.SkipAt(index).IsSafe()));
+
+        Console.WriteLine($"{safeCount0} {safeCount1}");
+    }
+
+    static bool IsSafe(this IEnumerable<int> source) =>
+        source.Pairwise((a, b) => b - a).All(d => d is >= +1 and <= +3) ||
+        source.Pairwise((a, b) => b - a).All(d => d is <= -1 and >= -3);
+
+    static IEnumerable<int> SkipAt(this IEnumerable<int> source, int index) =>
+        source.Where((_,i) => i != index);
+}
