5e6a362e847338fe69285901d687cc479faa8d84
timpengu <tim@peugniez.com>
2024-12-06 13:17:06 +0000

    Day 06: refactor to use Coord type and improve console visualisation

---
+++ AoC-2024/AoC-2024-06/Coord.cs
@@ -0,0 +1,17 @@
+﻿internal record struct Coord(int X, int Y) : IComparable<Coord>
+{
+    public static implicit operator Coord((int X, int Y) tuple) => new Coord(tuple.X, tuple.Y);
+    public static Coord operator +(Coord a, Coord b) => new Coord(a.X + b.X, a.Y + b.Y);
+    public override string ToString() => $"({X},{Y})";
+
+    public int CompareTo(Coord other)
+    {
+        int compareY = Y.CompareTo(other.Y);
+        if (compareY != 0) return compareY;
+
+        int compareX = X.CompareTo(other.X);
+        if (compareX != 0) return compareX;
+
+        return 0;
+    }
+}
--- AoC-2024/AoC-2024-06/Program.cs
+++ AoC-2024/AoC-2024-06/Program.cs
@@ -1,21 +1,26 @@
 ﻿
+using MoreLinq;
 using System.Collections.Immutable;
+
+int verboseLevel = 0; // 0..2
 
 IList<string> lines = File.ReadLines("input.txt").ToList();
 int xs = lines.Select(x => x.Length).Distinct().Single();
 int ys = lines.Count;
 
-int x = -1, y = -1;
-IImmutableSet<(int, int)> blocks = ImmutableHashSet.Create<(int, int)>();
+Coord zInit = (-1, -1);
+Coord dzInit = (0, -1);
 
-for (int ix = 0; ix < xs; ix++)
+IImmutableSet<Coord> blocks = ImmutableHashSet.Create<Coord>();
+
+for (int iy = 0; iy < ys; iy++)
 {
-    for (int iy = 0; iy < ys; iy++)
+    for (int ix = 0; ix < xs; ix++)
     {
         switch (lines[iy][ix])
         {
             case '^':
-                (x, y) = (ix, iy);
+                zInit = (ix, iy);
                 break;
 
             case '#':
@@ -28,70 +33,17 @@ for (int ix = 0; ix < xs; ix++)
     }
 }
 
-(int Moves, int DistinctPositions, bool HasLooped) Patrol(IImmutableSet<(int, int)> blocks, int x, int y, int dx = 0, int dy = -1)
-{
-    bool IsInRange(int x, int y) => x >= 0 && x < xs && y >= 0 && y < ys;
-
-    bool hasLooped = false;
-    int move = 0;
-    HashSet<(int X, int Y, int dX, int dY)> statesVisited = new();
-    while (IsInRange(x, y))
-    {
-        if (!statesVisited.Add((x, y, dx, dy)))
-        {
-            hasLooped = true;
-            break;
-        }
-
-        // Console.WriteLine($"\nMove {statesVisited.Count}:");
-        // ConsoleWriteMap(blocks, x, y, dx, dy);
-        // Console.WriteLine();
-
-        (int x2, int y2) = (x + dx, y + dy); // move forward
-        
-        while (blocks.Contains((x2,y2)))
-        {
-            (dx, dy) = (-dy, dx); // rotate direction vector
-            (x2, y2) = (x + dx, y + dy); // turn right
-        }
-
-        (x, y) = (x2, y2);
-    }
-
-    int distinctPositions = statesVisited.DistinctBy(s => (s.X, s.Y)).Count();
-    return (statesVisited.Count, distinctPositions, hasLooped);
-}
-
-void ConsoleWriteMap(IImmutableSet<(int, int)> blocks, int x, int y, int dx = 0, int dy = -1)
-{
-    for (int iy = 0; iy < ys; iy++)
-    {
-        for (int ix = 0; ix < xs; ix++)
-        {
-            Console.Write(
-                (ix, iy) == (x, y)
-                    ? (dx, dy) switch
-                                {
-                                    (0, -1) => '^',
-                                    (+1, 0) => '>',
-                                    (0, +1) => 'v',
-                                    (-1, 0) => '<',
-                                    _ => '@'
-                                }
-                    : blocks.Contains((ix, iy)) ? '#' : '.'
-            );
-        }
-        Console.WriteLine();
-    }
-}
-
-// ConsoleWriteMap(blocks, x, y);
-
 // Part 1
 {
-    var result = Patrol(blocks, x, y);
-    Console.WriteLine($"\nMoves: {result.Moves}");
-    Console.WriteLine($"Distinct positions: {result.DistinctPositions}");
+    var patrol = Patrol(blocks, zInit, dzInit);
+
+    Console.WriteLine($"Left map after {patrol.Moves.Count} moves");
+    Console.WriteLine($"Distinct positions visited: {CountDistinctPositions(patrol.Moves)}");
+    if (verboseLevel >= 2)
+    {
+        ConsoleWriteMap(blocks, patrol.Moves);
+    }
+    Console.WriteLine();
 }
 
 // Part 2
@@ -101,24 +53,101 @@ void ConsoleWriteMap(IImmutableSet<(int, int)> blocks, int x, int y, int dx = 0,
     {
         for (int ix = 0; ix < xs; ix++)
         {
-            if ((ix,iy) == (x,y) || blocks.Contains((ix, iy)))
+            Coord z = (ix, iy);
+
+            if (z == zInit || blocks.Contains(z))
             {
-                continue;
+                continue; // don't place a block on top of the guard or existing blocks
             }
 
-            var withExtraBlock = blocks.Add((ix, iy));
-            var result = Patrol(withExtraBlock, x, y);
-            if (result.HasLooped)
+            IImmutableSet<Coord> withExtraBlock = blocks.Add(z);
+
+            var patrol = Patrol(withExtraBlock, zInit, dzInit);
+            if (patrol.HasLooped)
             {
                 ++loopingSolutions;
 
-                Console.WriteLine($"\nWith extra block @ ({ix},{iy}):");
-                Console.WriteLine($"Looped after {result.Moves} moves");
-                Console.WriteLine($"Distinct positions: {result.DistinctPositions}");
-                // ConsoleWriteMap(withExtraBlock, x, y);
+                if (verboseLevel >= 1)
+                {
+                    Console.WriteLine($"With extra block @ {z}:");
+                    Console.WriteLine($"Looped after {patrol.Moves.Count} moves");
+                    Console.WriteLine($"Distinct positions: {CountDistinctPositions(patrol.Moves)}");
+
+                    if (verboseLevel >= 2)
+                    {
+                        ConsoleWriteMap(withExtraBlock, patrol.Moves, z);
+                    }
+
+                    Console.WriteLine();
+                }
             }
         }
     }
 
-    Console.WriteLine($"\nLooping solutions: {loopingSolutions}");
+    Console.WriteLine($"Looping solutions with one extra block: {loopingSolutions}");
+}
+
+bool IsInRange(Coord z) => z.X >= 0 && z.X < xs && z.Y >= 0 && z.Y < ys;
+int CountDistinctPositions(IEnumerable<(Coord Z, Coord dZ)> moves) => moves.DistinctBy(s => s.Z).Count();
+
+(IReadOnlyCollection<(Coord Z, Coord dZ)> Moves, bool HasLooped) Patrol(IImmutableSet<Coord> blocks, Coord z, Coord dz)
+{
+    List<(Coord Z, Coord dZ)> moves = new(); // ordered list of moves
+    HashSet<(Coord Z, Coord dZ)> visited = new(); // hash set of moves for loop check (unfortunately no ordered hash set in .NET)
+
+    bool hasLooped = false;
+
+    while (IsInRange(z))
+    {
+        moves.Add((z, dz));
+
+        if (!visited.Add((z, dz))) // already been on this path?
+        {
+            hasLooped = true;
+            break;
+        }
+
+        while (blocks.Contains(z + dz)) // next position is blocked?
+        {
+            dz = (-dz.Y, dz.X); // rotate direction vector
+        }
+
+        z += dz; // move in current direction
+    }
+
+    return (moves, hasLooped);
+}
+
+void ConsoleWriteMap(IImmutableSet<Coord> blocks, IReadOnlyCollection<(Coord Z, Coord dZ)> moves, Coord? zExtraBlock = null)
+{
+    Coord zStart = moves.Any() ? moves.First().Z : (-1, -1);
+    ILookup<Coord, Coord> directionsByPosition = moves
+        .Concat(moves.Pairwise((m1, m2) => (m1.Z, m2.dZ))) // include new direction if it changes
+        .ToLookup(m => m.Z, m => m.dZ);
+
+    for (int iy = 0; iy < ys; iy++)
+    {
+        for (int ix = 0; ix < xs; ix++)
+        {
+            Coord z = (ix, iy);
+
+            List<Coord> dirs = directionsByPosition[z].ToList();
+            bool hasMoveH = dirs.Any(dz => dz.X != 0);
+            bool hasMoveY = dirs.Any(dz => dz.Y != 0);
+
+            Console.Write(
+                z == zStart ? '^' :
+                z == zExtraBlock ? 'O' :
+                blocks.Contains(z) ? '#' :
+                (hasMoveH, hasMoveY) switch
+                {
+                    (true, false) => '-',
+                    (false, true) => '|',
+                    (true, true) => '+',
+                    _ => '.'
+                });
+        }
+
+        Console.WriteLine();
+    }
 }
