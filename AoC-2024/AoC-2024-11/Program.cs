5ddfe9399381eae3692d191335c5cbc214e6c865
timpengu <tim@peugniez.com>
2024-12-11 18:29:31 +0000

    Day 11: changed to recursive expansion with memoization

--- AoC-2024/AoC-2024-11/Program.cs
+++ AoC-2024/AoC-2024-11/Program.cs
@@ -1,21 +1,78 @@
 ﻿using MoreLinq;
+using System.Diagnostics;
 
-List<ulong> values =
-    File.ReadLines("input.txt")
-    .Single()
-    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-    .Select(ulong.Parse)
-    .ToList();
-
-IEnumerable<ulong> Expand(IEnumerable<ulong> values)
+internal static class Program
 {
-    foreach (ulong value in values)
+    const int maxDisplayValues = 1000;
+
+    private static Dictionary<(ulong Value, int Expansions), long> memoizedCounts = new();
+
+    private static void Main(string[] args)
+    {
+        List<ulong> values =
+            File.ReadLines("input.txt")
+            .Single()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(ulong.Parse)
+            .ToList();
+
+        Console.WriteLine("Initial arrangement:");
+        Console.WriteLine(string.Join(' ', values));
+
+        for (int blinks = 1; blinks <= 75; ++blinks)
+        {
+            Console.WriteLine($"\nAfter blink {blinks}:");
+
+            long count = CountExpanded(values, blinks);
+
+            if (blinks <= 25)
+            {
+                List<ulong> expandedValues = values.Expand(blinks).ToList();
+                Debug.Assert(expandedValues.Count == count);
+
+                Console.Write(String.Join(' ', expandedValues.Take(maxDisplayValues)));
+                Console.WriteLine(expandedValues.Count > maxDisplayValues ? "..." : "");
+
+                Console.WriteLine($"Log10 distribution: {string.Join(' ', expandedValues.GetDigitsFrequency())}");
+            }
+
+            Console.WriteLine($"Memoized counts: {memoizedCounts.Count}");
+            Console.WriteLine($"Number of stones: {count} ({count:e0})");
+        }
+    }
+
+    static long CountExpanded(this IEnumerable<ulong> values, int expansions) => values.Sum(value => value.CountExpanded(expansions));
+    static long CountExpanded(this ulong value, int expansions)
+    {
+        if (expansions == 0)
+        {
+            return 1;
+        }
+
+        if (!memoizedCounts.TryGetValue((value, expansions), out long count))
+        {
+            count = value.Expand().CountExpanded(expansions - 1);
+            memoizedCounts.Add((value, expansions), count);
+        }
+
+        return count;
+    }
+
+    static IEnumerable<ulong> Expand(this IEnumerable<ulong> values, int expansions) => values.SelectMany(value => value.Expand(expansions));
+    static IEnumerable<ulong> Expand(this ulong value, int expansions)
+    {
+        return expansions == 0
+            ? [value]
+            : value.Expand().Expand(expansions - 1);
+    }
+
+    static IEnumerable<ulong> Expand(this ulong value)
     {
         if (value == 0)
         {
             yield return 1;
         }
-        else if (TrySplit(value, out ulong a, out ulong b))
+        else if (value.TrySplit(out ulong a, out ulong b))
         {
             yield return a;
             yield return b;
@@ -25,47 +82,37 @@ IEnumerable<ulong> Expand(IEnumerable<ulong> values)
             yield return value * 2024;
         }
     }
-}
 
-bool TrySplit(ulong value, out ulong a, out ulong b)
-{
-    uint d = GetDigits(value);
-    if (d % 2 == 0)
+    static bool TrySplit(this ulong value, out ulong a, out ulong b)
     {
-        ulong mod = (ulong)Math.Pow(10, d / 2);
-        a = value / mod;
-        b = value % mod;
-        return true;
+        uint d = value.GetDigits();
+        if (d % 2 == 0)
+        {
+            ulong mod = (ulong)Math.Pow(10, d / 2);
+            a = value / mod;
+            b = value % mod;
+            return true;
+        }
+        else
+        {
+            a = 0;
+            b = 0;
+            return false;
+        }
     }
-    else
+
+    static uint GetDigits(this ulong value) => value switch
     {
-        a = 0;
-        b = 0;
-        return false;
+        < 10L => 1,
+        < 100L => 2,
+        _ => (uint)Math.Log10(value) + 1
+    };
+
+    static IEnumerable<int> GetDigitsFrequency(this IEnumerable<ulong> values)
+    {
+        Dictionary<int, int> digitsFrequency = values.CountBy(GetDigits).ToDictionary(kvp => (int)kvp.Key, kvp => kvp.Value);
+        return Enumerable.Range(1, digitsFrequency.Keys.Max())
+            .Select(digits => digitsFrequency.TryGetValue(digits, out int freq) ? freq : 0)
+            .ToList();
     }
-}
-
-uint GetDigits(ulong value) => value switch
-{
-    < 10L => 1,
-    < 100L => 2,
-    _ => (uint)Math.Log10(value) + 1
-};
-
-Console.WriteLine("Initial arrangement:");
-Console.WriteLine(String.Join(' ', values));
-
-List<List<ulong>> allValues = [values];
-for (int blink = 1; blink <= 75; ++blink)
-{
-    allValues = allValues
-        .AsParallel()
-        .SelectMany(l => l)
-        .Batch(100000)
-        .Select(l => Expand(l).ToList())
-        .ToList();
-
-    Console.WriteLine($"\nAfter blink {blink}:");
-    // Console.WriteLine(String.Join(' ', allValues.SelectMany(l => l)));
-    Console.WriteLine($"Number of stones: {allValues.Sum(l => l.Count)} (in {allValues.Count} lists)");
 }
