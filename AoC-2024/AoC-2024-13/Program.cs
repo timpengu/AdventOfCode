cc1cafa4f3d405a18fd060e473d40a09c2f9a2fb
timpengu <tim@peugniez.com>
2024-12-14 00:00:58 +0000

    Day 13: refactored to simplify and run both parts

--- AoC-2024/AoC-2024-13/Program.cs
+++ AoC-2024/AoC-2024-13/Program.cs
@@ -1,67 +1,53 @@
 ﻿using MoreLinq;
 using System.Text.RegularExpressions;
 
-const int aCost = 3;
-const int bCost = 1;
-
 Coord offset = (10000000000000L, 10000000000000L);
 
-List<(Coord A, Coord B, Coord Prize)> input = new(File.ReadLines("input.txt")
+List<Input> inputs = new(
+    File.ReadLines("input.txt")
     .Where(line => line.Trim().Length > 0)
     .Batch(3)
-    .Select(l => (
-        A: ParseButton(l[0], "A"),
-        B: ParseButton(l[1], "B"),
-        Prize: ParsePrize(l[2])
-    ))
-);
+    .Select(l => new Input(
+        ParseButton(l[0], "A"),
+        ParseButton(l[1], "B"),
+        ParsePrize(l[2])
+    )));
 
-long totalCost = 0;
+// Part 1
+List<(Input Input, Win Win)> results = inputs
+    .SelectMany(FindWins, (input, win) => (input, win))
+    .ToList();
 
-foreach(var item in input)
+foreach ((Input input, Win win) in results)
 {
-    List<Win> wins = FindWins2(item.A, item.B, item.Prize + offset).ToList();
-    foreach(var win in wins)
-    {
-        Console.WriteLine($"A:{item.A}*{win.A} B:{item.B}*{win.B} = Prize:{item.Prize + offset} @ Cost:{GetCost(win)}");
-    }
-    if (wins.Count > 0)
-    {
-        totalCost += wins.Min(GetCost);
-    }
+    Console.WriteLine($"A:{input.A}*{win.ACount} B:{input.B}*{win.BCount} = Prize:{input.Prize} @ Cost:{win.Cost}");
 }
+Console.WriteLine($"Total cost {results.Sum(r => r.Win.Cost)}\n");
 
-Console.WriteLine($"Total cost {totalCost}");
+// Part 2
+List<(Input Input, Win Win)> results2 = inputs
+    .Select(input => new Input(input.A, input.B, input.Prize + offset))
+    .SelectMany(FindWins, (input, win) => (input, win))
+    .ToList();
 
-IEnumerable<Win> FindWins1(Coord a, Coord b, Coord prize)
+foreach ((Input input, Win win) in results2)
 {
-    for (long aCount = 0; aCount <= GetMaxCount(a, prize); ++aCount)
-    {
-        Coord remainder = prize - aCount * a;
-        if (remainder.TryDiv(b, out long bCount))
-        {
-            yield return new Win(aCount, bCount);
-        }
-    }
+    Console.WriteLine($"A:{input.A}*{win.ACount} B:{input.B}*{win.BCount} = Prize:{input.Prize} @ Cost:{win.Cost}");
 }
+Console.WriteLine($"Total cost {results2.Sum(r => r.Win.Cost)}\n");
 
-IEnumerable<Win> FindWins2(Coord a, Coord b, Coord prize)
+IEnumerable<Win> FindWins(Input input)
 {
-    // ohhhh shit oh fuck I mean solve for (an,bn) in: an * a + bn * b == prize
-    long pd = prize.Y * b.X - prize.X * b.Y;
-    long ad = a.Y * b.X - a.X * b.Y;
-    long an = pd / ad;
-    long bn = (prize.X - an * a.X) / b.X;
+    // argggh... solve for (an,bn) in (an * A + bn * B == Prize)
+    (Coord a, Coord b, Coord p) = (input.A, input.B, input.Prize);
+    long an = (p.Y * b.X - p.X * b.Y) / (a.Y * b.X - a.X * b.Y);
+    long bn = (p.X - an * a.X) / b.X;
     Coord z = an * a + bn * b;
-    if (z == prize)
+    if (z == p)
     {
         yield return new Win(an, bn);
     }
 }
-
-long GetCost(Win move) => move.A * aCost + move.B * bCost;
-long GetMaxCount(Coord z, Coord prize) => Min(prize.X / z.X, prize.Y / z.Y);
-long Min(params long[] values) => values.Min();
 
 Coord ParseButton(string line, string name)
 {
@@ -90,7 +76,14 @@ Coord ParsePrize(string line)
     throw new FormatException($"Failed to parse line: '{line}'");
 }
 
-record struct Win(long A, long B);
+record struct Input(Coord A, Coord B, Coord Prize);
+record struct Win(long ACount, long BCount)
+{
+    public const int ACost = 3;
+    public const int BCost = 1;
+    public long Cost => ACount * ACost + BCount * BCost;
+}
+
 record struct Coord(long X, long Y)
 {
     public static implicit operator Coord((long X, long Y) tuple) => new Coord(tuple.X, tuple.Y);
@@ -98,20 +91,5 @@ record struct Coord(long X, long Y)
     public static Coord operator -(Coord a, Coord b) => new Coord(a.X - b.X, a.Y - b.Y);
     public static Coord operator *(Coord a, long f) => f * a;
     public static Coord operator *(long f, Coord a) => new Coord(f * a.X, f * a.Y);
-
-    public bool TryDiv(Coord divisor, out long result)
-    {
-        if (X / divisor.X == Y / divisor.Y &&
-            X % divisor.X == 0 &&
-            Y % divisor.Y == 0)
-        {
-            result = X / divisor.X;
-            return true;
-        }
-
-        result = 0;
-        return false;
-    }
-
     public override string ToString() => $"({X},{Y})";
 }
