f7eab638e03b14853e12564b9e59a9aa053c2f04
timpengu <tim@peugniez.com>
2024-12-22 11:30:37 +0000

    Day 22: added solution for part 1

---
+++ AoC-2024/AoC-2024-22/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using MoreLinq;
+
+IList<ulong> seeds = File.ReadLines("input.txt").Select(ulong.Parse).ToList();
+
+ulong modulo = 16777216;
+int iterations = 2000;
+
+ulong total = 0;
+foreach(ulong seed in seeds)
+{
+    ulong value = Generate(seed, iterations);
+    total += value;
+
+    Console.WriteLine($"{seed}: {value}");
+}
+
+Console.WriteLine($"Total of {seeds.Count} values after {iterations} iterations: {total}");
+
+ulong Generate(ulong seed, int iterations)
+{
+    ulong value = seed;
+    for (int i = 0; i < iterations; ++i)
+    {
+        value = GenerateNext(value);
+    }
+    return value;
+}
+
+ulong GenerateNext(ulong value)
+{
+    value = Mix(value, value << 6);
+    value = Mix(value, value >> 5);
+    value = Mix(value, value << 11);
+    return value;
+}
+
+ulong Mix(ulong a, ulong b) => (a ^ b) % modulo;
