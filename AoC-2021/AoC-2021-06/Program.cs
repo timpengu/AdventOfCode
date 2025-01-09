fb2eaf562045f55b5a8fe8df6208c5ae6c714d12
timpengu <tim@peugniez.com>
2025-01-09 19:25:11 +0000

    AoC-2021-06: added AoC-2021 with day 06 solution (lanternfish)

---
+++ AoC-2021/AoC-2021-06/Program.cs
@@ -0,0 +1,35 @@
+﻿
+const int RepoInterval = 7;
+const int RepoMaturity = 9;
+
+List<int> values =
+    File.ReadLines("input.txt")
+    .Single()
+    .Split(',', StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+long[] dist =
+    Enumerable.Range(0, RepoMaturity)
+    .Select(x => (long)values.Count(v => v == x))
+    .ToArray();
+
+Console.WriteLine($"Initial state: [{dist.Sum()}] {String.Join(',', dist)}");
+
+for (int day = 1; day <= 256; ++day)
+{
+    AdvanceDay(dist);
+
+    Console.WriteLine($"After {day} days: [{dist.Sum()}] {String.Join(',', dist)}");
+}
+
+void AdvanceDay(long[] dist)
+{
+    long matureCount = dist[0];
+    for(int i = 1; i < RepoMaturity; ++i)
+    {
+        dist[i - 1] = dist[i];
+    }
+    dist[RepoInterval - 1] += matureCount;
+    dist[RepoMaturity - 1] = matureCount;
+}
