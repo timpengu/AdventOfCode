264ec9780be21640e694a5f3f2f52b06a04cd72b
timpengu <tim@peugniez.com>
2025-03-28 23:29:50 +0000

    AoC-2019-15: extended for part 2, generate entire map and BFS separately

--- AoC-2019/AoC-2019-15/Program.cs
+++ AoC-2019/AoC-2019-15/Program.cs
@@ -1,4 +1,5 @@
 ﻿using IntCode;
+using System.Diagnostics;
 
 internal static class Program
 {
@@ -9,63 +10,63 @@ internal static class Program
             .Select(long.Parse)
             .ToList();
 
-        List<Direction> path = program.FindShortestPath();
-        Console.WriteLine($"Shortest path [{path.Count}]: {String.Concat(path.Select(d => d.Char))}");
+        Coord zOrigin = (0, 0);
+        IReadOnlyDictionary<Coord, Block> map = program.GenerateMap(zOrigin);
+        Visualise(map);
 
-        // TODO: explore entire region and return map instead of shortest path
-        // TODO: add map visualisation
-        // TODO: run SPF on map instead of Computer
-        // TODO: return SPF from oxygen to origin (part 1)
-        // TODO: return maximum path from oxygen (part 2)
+        var zOxygen = map.Single(m => m.Value == Block.Oxygen).Key;
+        (int distOxygen, int distMax) = GetDistances(map, zOxygen, zOrigin);
+
+        Console.WriteLine($"Shortest distance to oxygen: {distOxygen}");
+        Console.WriteLine($"Maximum distance from oxygen: {distMax}");
     }
 
-    private static List<Direction> FindShortestPath(this IReadOnlyCollection<long> program)
+    private static IReadOnlyDictionary<Coord,Block> GenerateMap(this IReadOnlyCollection<long> program, Coord zStart)
     {
+        Dictionary<Coord, Block> map = new() { [zStart] = Block.Space };
         Dictionary<Coord, Direction> pathDirections = new();
         HashSet<Coord> visited = new();
-        Queue<Coord> queue = new([(0,0)]);
+        Queue<Coord> queue = new([zStart]);
         while (queue.TryDequeue(out Coord zCurr))
         {
-            if (visited.Add(zCurr))
+            if (!visited.Add(zCurr))
             {
-                IReadOnlyCollection<Direction> pathToCurr = pathDirections.GetPathTo(zCurr);
+                continue;
+            }
 
-                foreach(Direction direction in Direction.All)
+            IReadOnlyCollection<Direction> pathToCurr = pathDirections.GetPathTo(zCurr);
+
+            foreach (Direction direction in Direction.All)
+            {
+                Coord zNext = zCurr + direction.dZ;
+
+                if (visited.Contains(zNext) || pathDirections.ContainsKey(zNext))
                 {
-                    Coord zNext = zCurr + direction.dZ;
-                    if (visited.Contains(zNext) || pathDirections.ContainsKey(zNext))
-                    {
-                        // already visited zNext or found a shorther path
-                        continue;
-                    }
+                    // already visited zNext or found a shorther path
+                    continue;
+                }
 
-                    // execute the path and get the output for the next step
-                    IEnumerable<Direction> pathToNext = pathToCurr.Append(direction);
-                    long[] inputs = pathToNext.Select(d => d.Input).ToArray();
-                    Computer<long> computer = new(program, inputs);
-                    long nextOutput = computer.ExecuteOutputs().Skip(pathToCurr.Count).First();
+                // TODO: refactor the Computer to the outer loop and push each direction to explore onto the queue
+                // execute the path and get the output for the next step
+                IEnumerable<Direction> pathToNext = pathToCurr.Append(direction);
+                long[] inputs = pathToNext.Select(d => d.Input).ToArray();
+                Computer<long> computer = new(program, inputs);
+                long nextOutput = computer.ExecuteOutputs().Skip(pathToCurr.Count).First();
 
-                    switch(nextOutput)
-                    {
-                        case 0: // can't move in this direction
-                            break;
+                Block block = nextOutput.ToBlock();
 
-                        case 1: // moved in this direction
-                            pathDirections.Add(zNext, direction);
-                            queue.Enqueue(zNext);
-                            break;
+                Debug.Assert(!map.TryGetValue(zNext, out Block blockExisting) || block == blockExisting);
+                map[zNext] = block;
 
-                        case 2: // found the target by the shortest path
-                            return pathToNext.ToList();
-
-                        default:
-                            throw new Exception($"Invalid output: {nextOutput}");
-                    }
+                if (block != Block.Wall) // can move in this direction?
+                {
+                    pathDirections.Add(zNext, direction);
+                    queue.Enqueue(zNext);
                 }
             }
         }
 
-        return []; // no path found
+        return map;
     }
 
     private static IReadOnlyCollection<Direction> GetPathTo(
@@ -78,6 +79,72 @@ internal static class Program
             directions.Push(direction);
         }
         return directions;
+    }
+
+    private static (int TargetDistance, int MaxDistance) GetDistances(IReadOnlyDictionary<Coord, Block> map, Coord zStart, Coord zTarget)
+    {
+        HashSet<Coord> path = map
+            .Where(m => m.Value != Block.Wall)
+            .Select(m => m.Key)
+            .ToHashSet();
+
+        Dictionary<Coord, int> dist = new() { [zStart] = 0 };
+        Queue<Coord> queue = new([zStart]);
+
+        while (queue.TryDequeue(out Coord zCurr))
+        {
+            if (path.Remove(zCurr)) // doubles as unvisited set
+            {
+                int distCurr = dist[zCurr];
+
+                IEnumerable<Coord> unvisitedNeighbours =
+                    Direction.All
+                    .Select(dir => zCurr + dir.dZ)
+                    .Where(zn => path.Contains(zn))
+                    .Where(zn => !dist.ContainsKey(zn));
+
+                foreach (Coord zNext in unvisitedNeighbours)
+                {
+                    dist.Add(zNext, distCurr + 1);
+                    queue.Enqueue(zNext);
+                }
+            }
+        }
+
+        int distTarget = dist[zTarget];
+        int distMax = dist.Values.Max();
+
+        return (distTarget, distMax);
+    }
+
+    private static void Visualise(IReadOnlyDictionary<Coord, Block> map)
+    {
+        var xMin = map.Keys.Min(z => z.X);
+        var yMin = map.Keys.Min(z => z.Y);
+        var xMax = map.Keys.Max(z => z.X);
+        var yMax = map.Keys.Max(z => z.Y);
+        for (int y = yMin; y <= yMax; ++y)
+        {
+            for (int x = xMin; x <= xMax; ++x)
+            {
+                Coord z = (x, y);
+                Block block = map.TryGetValue(z, out var b) ? b : Block.Unknown;
+                char c = block switch
+                {
+                    Block.Wall => '#',
+                    Block.Space => '.',
+                    Block.Oxygen => 'O',
+                    Block.Unknown => ' ',
+                    _ => throw new Exception($"Invalid block: {block}")
+                };
+                if (z == (0,0))
+                {
+                    c = 'D';
+                }
+                Console.Write(c);
+            }
+            Console.WriteLine();
+        }
     }
 
     private record struct Direction(char Char, long Input, Coord dZ)
@@ -94,5 +161,20 @@ internal static class Program
             Direction.West,
             Direction.East
         };
+    }
+
+    private static Block ToBlock(this long output)
+    {
+        var block = (Block)output;
+        return Enum.IsDefined<Block>(block) ? block
+            : throw new Exception($"Invalid block: {block}");
+    }
+
+    private enum Block
+    {
+        Unknown = -1,
+        Wall = 0,
+        Space = 1,
+        Oxygen = 2
     }
 }
