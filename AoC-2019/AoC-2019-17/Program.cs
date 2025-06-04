6185cd65ffb238497b147748d5cbbe71d7251b9d
timpengu <tim@peugniez.com>
2025-06-04 16:04:32 +0100

    AoC-2019-17: use Computer<int> (long is not necessary)

--- AoC-2019/AoC-2019-17/Program.cs
+++ AoC-2019/AoC-2019-17/Program.cs
@@ -10,46 +10,51 @@ internal static class Program
 
     public static void Main(string[] args)
     {
-        List<long> program = string.Concat(File.ReadLines("input.txt"))
+        List<int> program = string.Concat(File.ReadLines("input.txt"))
             .Split(',', StringSplitOptions.RemoveEmptyEntries)
-            .Select(long.Parse)
+            .Select(int.Parse)
             .ToList();
 
         Part1(program);
         Part2(program);
     }
 
-    private static void Part1(IEnumerable<long> program)
+    private static void Part1(IEnumerable<int> program)
     {
-        var computer = new Computer<long>(program);
+        var computer = new Computer<int>(program);
 
         IEnumerable<char> output = computer.ExecuteOutputs().Select(ToChar);
         string[] lines = String.Concat(output).Split('\n', StringSplitOptions.RemoveEmptyEntries);
+        
+        List<Coord> intersects = GetIntersections(lines);
+
+        Console.WriteLine($"Intersections: {String.Join(' ', intersects)}");
+        Console.WriteLine($"Alignment sum: {intersects.Sum(z => z.X * z.Y)}");
+    }
+
+    private static List<Coord> GetIntersections(string[] lines)
+    {
         Coord size = (lines.Max(l => l.Length), lines.Length);
+        Coord[] IntersectOffsets = [(0, 0), (0, 1), (1, 0), (-1, 0), (0, -1)];
 
         bool IsInRange(Coord z) => z.X >= 0 && z.X < size.X && z.Y >= 0 && z.Y < size.Y;
         char GetChar(Coord z) => IsInRange(z) ? lines[z.Y][z.X] : Ascii.Null;
         bool IsScaffold(Coord z) => GetChar(z) == Ascii.Scaffold;
-
-        Coord[] IntersectOffsets = [(0, 0), (0, 1), (1, 0), (-1, 0), (0, -1)];
         bool IsIntersect(Coord z) => IntersectOffsets.All(dz => IsScaffold(z + dz));
 
-        List<Coord> intersects = new(
+        return new List<Coord>(
             from x in Enumerable.Range(0, size.X)
             from y in Enumerable.Range(0, size.Y)
             let z = new Coord(x, y)
             where IsIntersect(z)
             select z
         );
-
-        Console.WriteLine($"Intersections: {String.Join(' ', intersects)}");
-        Console.WriteLine($"Alignment sum: {intersects.Sum(z => z.X * z.Y)}");
     }
 
-    private static void Part2(IEnumerable<long> program)
+    private static void Part2(IEnumerable<int> program)
     {
-        var inputQueue = new BlockingInputQueue<long>();
-        var computer = new Computer<long>(program, inputQueue);
+        var inputs = new BlockingInputQueue<int>();
+        var computer = new Computer<int>(program, inputs);
 
         computer[0] = 2; // wake the robot 🤖
 
@@ -61,19 +66,19 @@ internal static class Program
             {
                 foreach (char input in line.Append('\n'))
                 {
-                    inputQueue.Enqueue(input);
+                    inputs.Enqueue(input);
                 }
             }
         });
 
         // Execute program and write output to Console
         Console.WriteLine();
-        foreach (long output in computer.ExecuteOutputs())
+        foreach (int output in computer.ExecuteOutputs())
         {
             Console.Write(output.IsChar() ? output.ToChar() : $"Output: {output}\n");
         }
     }
 
-    private static bool IsChar(this long output) => output is >= Char.MinValue and <= Char.MaxValue;
-    private static char ToChar(this long output) => IsChar(output) ? (char) output : throw new Exception($"Invalid char: {output}");
+    private static bool IsChar(this int output) => output is >= Char.MinValue and <= Char.MaxValue;
+    private static char ToChar(this int output) => IsChar(output) ? (char) output : throw new Exception($"Invalid char: {output}");
 }
