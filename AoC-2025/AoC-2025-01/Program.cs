61d25677dbd0e572fed4b5ba278721fdf05b6102
timpengu <tim@peugniez.com>
2025-12-01 22:42:02 +0000

    AoC-2025: added AoC-2025 solution with day 1

---
+++ AoC-2025/AoC-2025-01/Program.cs
@@ -0,0 +1,34 @@
+﻿
+List<int> rotations =
+    File.ReadLines("input.txt")
+    .Select(s => s.Trim().Replace('R', '+').Replace('L', '-'))
+    .Select(int.Parse)
+    .ToList();
+
+int dial = 50;
+int range = 100;
+int z1 = 0;
+int z2 = 0;
+
+Console.WriteLine($"{dial}");
+
+foreach (int rot in rotations)
+{
+    int dz2 = rot >= 0
+        ? (dial + rot) / range
+        : (Inv(dial, range) - rot) / range;
+
+    dial = Mod(dial + rot, range);
+
+    int dz1 = dial == 0 ? 1 : 0;
+
+    z1 += dz1;
+    z2 += dz2;
+
+    Console.WriteLine($"{rot:+0;-0} => {dial}, dz1={dz1}, dz2={dz2}");
+}
+
+Console.WriteLine($"\nz1={z1}, z2={z2}");
+
+static int Inv(int x, int m) => Mod(m - x, m);
+static int Mod(int x, int m) => (x % m + m) % m;
---
+++ AoC-2025/AoC-2025-02/Program.cs
@@ -0,0 +1,8 @@
+﻿
+List<string> lines = File.ReadLines("input.txt").ToList();
+
+Console.WriteLine(lines.Count);
+
+internal static class Extensions
+{
+}
