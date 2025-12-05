d65a0ddbd5059a16199f5949a3c2682efba59c03
timpengu <tim@peugniez.com>
2025-12-05 10:10:34 +0000

    AoC-2025: day 05 added new algorithm and simplified input

--- AoC-2025/AoC-2025-05/Program.cs
+++ AoC-2025/AoC-2025-05/Program.cs
@@ -2,20 +2,19 @@
 var items = new List<long>();
 var ranges = new List<(long First, long Last)>();
 
-var lines = File.ReadLines("input.txt").Select(line => line.Trim()).Where(line => line.Length > 0);
-foreach (string line in lines)
+var lines = File.ReadLines("input.txt")
+    .Select(line => line.Trim().Split('-', 2, StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray())
+    .Where(line => line.Any());
+
+foreach (long[] line in lines)
 {
-    int sep = line.IndexOf('-');
-    if (sep >= 0)
+    if (line.Length > 1)
     {
-        long first = long.Parse(line[..sep]);
-        long last = long.Parse(line[(sep+1)..]);
-        ranges.Add((first, last));
+        ranges.Add((line[0], line[1]));
     }
     else
     {
-        long item = long.Parse(line);
-        items.Add(item);
+        items.Add(line[0]);
     }
 }
 
@@ -29,25 +28,7 @@ foreach(long i in items)
 
 Console.WriteLine();
 
-var merged = new List<(long First, long Last)>(ranges.Count);
-foreach (var r in ranges)
-{
-    int i = merged.FindLastIndex(m => m.First <= r.First); // range comes after i (or overlaps)
-    int j = merged.FindIndex(m => m.Last >= r.Last); // range comes before j (or overlaps)
-
-    bool iOverlap = i >= 0 && merged[i].Last >= r.First; // range overlaps i?
-    bool jOverlap = j >= 0 && merged[j].First <= r.Last; // range overlaps j?
-
-    long first = iOverlap ? merged[i].First : r.First; // first of new range
-    long last = jOverlap ? merged[j].Last : r.Last; // last of new range
-
-    merged.RemoveInclusiveRange(iOverlap ? i : i + 1, jOverlap ? j : j - 1); // remove overlapping ranges
-    merged.Insert(iOverlap ? i : i + 1, (first, last)); // insert new range
-
-    Console.WriteLine($"Adding {r.First}-{r.Last}: {String.Join(',', merged.Select(c => $"{c.First}-{c.Last}"))}");
-}
-
-long totalInRange = merged.Sum(c => c.Last - c.First + 1);
+long totalInRange = ranges.Coalesce2().Sum(c => c.Last - c.First + 1);
 
 Console.WriteLine();
 Console.WriteLine($"Items in range: {itemsInRange}");
@@ -55,6 +36,58 @@ Console.WriteLine($"Total in range: {totalInRange}");
 
 static class Extensions
 {
+    public static IEnumerable<(long First, long Last)> Coalesce1(this IEnumerable<(long First, long Last)> ranges)
+    {
+        List<(long First, long Last)> merged = [];
+        foreach (var r in ranges)
+        {
+            int i = merged.FindLastIndex(m => m.First <= r.First); // range comes after i (or overlaps)
+            int j = merged.FindIndex(m => m.Last >= r.Last); // range comes before j (or overlaps)
+
+            bool iOverlap = i >= 0 && merged[i].Last >= r.First; // range overlaps i?
+            bool jOverlap = j >= 0 && merged[j].First <= r.Last; // range overlaps j?
+
+            long first = iOverlap ? merged[i].First : r.First; // first of new range
+            long last = jOverlap ? merged[j].Last : r.Last; // last of new range
+
+            merged.RemoveInclusiveRange(iOverlap ? i : i + 1, jOverlap ? j : j - 1); // remove overlapping ranges
+            merged.Insert(iOverlap ? i : i + 1, (first, last)); // insert new range
+
+            Console.WriteLine($"+{r.First}-{r.Last}: {String.Join(',', merged.Select(c => $"{c.First}-{c.Last}"))}");
+        }
+        return merged;
+    }
+
+    public static IEnumerable<(long First, long Last)> Coalesce2(this IEnumerable<(long First, long Last)> ranges)
+    {
+        var orderedRanges = ranges.OrderBy(r => r.First).GetEnumerator();
+        if (!orderedRanges.MoveNext()) yield break;
+
+        (long First, long Last) range = orderedRanges.Current;
+        Console.WriteLine($"{range.First}-{range.Last}");
+
+        while (orderedRanges.MoveNext())
+        {
+            var nextRange = orderedRanges.Current;
+            if (nextRange.First > range.Last)
+            {
+                yield return range;
+                range = nextRange;
+                Console.WriteLine($"{range.First}-{range.Last}");
+            }
+            else if (nextRange.Last > range.Last)
+            {
+                range = (range.First, nextRange.Last);
+                Console.WriteLine($"+{nextRange.First}-{nextRange.Last} => {range.First}-{range.Last}");
+            }
+            else // nextRange subsumed by range
+            {
+                Console.WriteLine($"({nextRange.First}-{nextRange.Last})");
+            }
+        }
+        yield return range;
+    }
+
     public static void RemoveInclusiveRange<T>(this List<T> l, int first, int last)
     {
         int count = last - first + 1;
