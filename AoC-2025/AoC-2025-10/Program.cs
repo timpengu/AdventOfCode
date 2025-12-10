d3707a205735b6c188b9444087f21222035a15c8
timpengu <tim@peugniez.com>
2025-12-10 06:07:53 +0000

    AoC-2025: day 10 draft

---
+++ AoC-2025/AoC-2025-10/Program.cs
@@ -0,0 +1,121 @@
+﻿using System.Diagnostics;
+
+List<Machine> machines = File.ReadLines("example.txt").Select(Machine.Parse).ToList();
+
+int indicatorTotal = 0;
+int joltageTotal = 0;
+
+foreach (var machine in machines)
+{
+    Console.WriteLine($"{machine}:");
+
+    List<int> indicatorSeq = FindIndicatorSequences(machine).First();
+    indicatorTotal += indicatorSeq.Count;
+
+    Console.WriteLine($"Indicator sequence: {String.Join(",", indicatorSeq)}");
+
+    List<int> joltageSeq = FindJoltageSequences(machine).First();
+    joltageTotal += joltageSeq.Count;
+
+    Console.WriteLine($"Joltage sequence: {String.Join(",", joltageSeq)}");
+}
+
+Console.WriteLine();
+Console.WriteLine($"Indicator total: {indicatorTotal}");
+Console.WriteLine($"Joltage total: {indicatorTotal}");
+
+IEnumerable<List<int>> FindIndicatorSequences(Machine machine)
+{
+    int targetIndicators = Enumerable.Range(0, machine.Indicators.Length).Where(i => machine.Indicators[i]).Aggregate(0, (m, v) => m | (1 << v));
+    List<int> buttonMasks = machine.Buttons.Select(b => b.Aggregate(0, (m, v) => m | (1 << v))).ToList();
+
+    var initialState = (
+        Indicators: 0,
+        Sequence: Enumerable.Empty<int>()
+    );
+
+    Queue<(int Indicators, IEnumerable<int> Sequence)> queue = new([initialState]);
+    HashSet<int> visited = [];
+
+    while (queue.TryDequeue(out var state))
+    {
+        if (visited.Add(state.Indicators))
+        {
+            // Console.WriteLine($" => {String.Join(",", state.Sequence)}");
+
+            if (state.Indicators == targetIndicators)
+            {
+                yield return state.Sequence.ToList();
+            }
+            else
+            {
+                for (int i = 0; i < buttonMasks.Count; i++)
+                {
+                    var nextState = (
+                        Indicators: state.Indicators ^ buttonMasks[i],
+                        Sequence: state.Sequence.Append(i)
+                    );
+                    queue.Enqueue(nextState);
+                }
+            }
+        }
+    }
+}
+
+IEnumerable<List<int>> FindJoltageSequences(Machine machine)
+{
+    int[] targetJoltages = machine.Joltages;
+    List<bool[]> buttons = machine.Buttons.Select(b => b.Aggregate(new bool[targetJoltages.Length], (a, v) => { a[v] = true; return a; })).ToList();
+
+    var initialState = (
+        Joltages: Enumerable.Repeat(0, targetJoltages.Length).ToArray(),
+        Sequence: Enumerable.Empty<int>()
+    );
+
+    Queue<(int[] Joltages, IEnumerable<int> Sequence)> queue = new([initialState]);
+
+    while (queue.TryDequeue(out var state))
+    {
+        //Console.WriteLine($"{{{String.Join(",", state.Joltages)}}} => {String.Join(", ", state.Sequence)}");
+
+        if (state.Joltages.SequenceEqual(targetJoltages))
+        {
+            yield return state.Sequence.ToList();
+        }
+        else if (state.Joltages.Zip(targetJoltages, (j, t) => (Joltage: j, Target: t)).All(v => v.Joltage <= v.Target))
+        {
+            for (int i = 0; i < buttons.Count; i++)
+            {
+                var nextState = (
+                    Joltages: state.Joltages.Zip(buttons[i], (j, b) => b ? j + 1 : j).ToArray(),
+                    Sequence: state.Sequence.Append(i)
+                );
+                queue.Enqueue(nextState);
+            }
+        }
+    }
+}
+
+record Machine(bool[] Indicators, List<int[]> Buttons, int[] Joltages)
+{
+    public static Machine Parse(string line)
+    {
+        // e.g. "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}"
+
+        List<string> a = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+        bool[] indicators = a[0].Trim('[', ']').Select(c => c == '#').ToArray();
+        int[] joltages = a[^1].Trim('{', '}').Split(",").Select(int.Parse).ToArray();
+        List<int[]> buttons = a[1..^1].Select(s => s.Trim('(', ')').Split(",").Select(int.Parse).ToArray()).ToList();
+
+        Debug.Assert(indicators.Length == joltages.Length);
+
+        return new Machine(indicators, buttons, joltages);
+    }
+
+    public override string ToString() => $"[{String.Join("", Indicators.Select(b => b ? '#' : '.'))}] {String.Join(", ", Buttons.Select(b => $"({string.Join(",", b)})"))} {{{string.Join(", ", Joltages)}}}";
+}
+
+internal static class Extensions
+{
+}
