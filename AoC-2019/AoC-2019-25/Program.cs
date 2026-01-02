6a86319ec921329d5d0502b0fcca539a12bccf0a
timpengu <tim@peugniez.com>
2026-01-02 19:29:46 +0000

    AoC-2019-25: added day 25 solution

---
+++ AoC-2019/AoC-2019-25/Program.cs
@@ -0,0 +1,42 @@
+﻿using IntCode;
+
+internal static class Program
+{
+    // TODO: Factor out ASCII-compatible computer interface
+    public static void Main(string[] args)
+    {
+        List<long> program = string.Concat(File.ReadLines("input.txt"))
+            .Split(',', StringSplitOptions.RemoveEmptyEntries)
+            .Select(long.Parse)
+            .ToList();
+
+        var inputs = new BlockingInputQueue<long>();
+        var computer = new Computer<long>(program, inputs);
+
+        // Read input from Console in a background task (thread)
+        Task.Run(() =>
+        {
+            string? line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                line = line.Split('#', 2, StringSplitOptions.TrimEntries).First();
+                if (String.IsNullOrWhiteSpace(line)) continue;
+                
+                foreach (char input in line.Append('\n'))
+                {
+                    inputs.Enqueue(input);
+                }
+            }
+        });
+
+        // Execute program and write output to Console
+        Console.WriteLine();
+        foreach (long output in computer.ExecuteOutputs())
+        {
+            Console.Write(output.IsChar() ? output.ToChar() : $"Output: {output}\n");
+        }
+    }
+
+    private static bool IsChar(this long output) => output is >= Char.MinValue and <= Char.MaxValue;
+    private static char ToChar(this long output) => IsChar(output) ? (char)output : throw new Exception($"Invalid char: {output}");
+}
